f7df90e5e7e5d56af11e9bbee78446e9e98baf83
yash tiwari <149458186+ctrly4sh@users.noreply.github.com>
2024-06-13 12:57:30 +0530

    tailwind Setup

---
+++ mee-u/src/components/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function Home(){
+    return(
+        <div>
+            <p className="text-4xl bg-orange-500 ">Mee-U , Lets Video chat !!!! </p>
+        </div>
+    )
+}
